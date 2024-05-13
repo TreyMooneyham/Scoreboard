4ad9a1a248ea28ab19cc0aa5a0aec05cf35d4498
Trey <viralaphinity@gmail.com>
2024-05-13 00:33:07 -0400

    Added "ImTextureID=ImU64" to the preprocessor flags as DX12 doesn't like it when you pass them 64-bit things in 32-bit compiling.

--- Scoreboard/main.cpp
+++ Scoreboard/main.cpp
@@ -147,8 +147,8 @@ int main(int, char**)
         if (bCharSheetVisible)
             charSheet(&bCharSheetVisible);
 
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
+        //if (show_demo_window)
+            //ImGui::ShowDemoWindow(&show_demo_window);
 
         /*
         // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
